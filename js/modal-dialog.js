cf1436cb4e151f8fe5fd01526f2ca6f11e29cbe6
Akuli <akuviljanen17@gmail.com>
2019-05-19 15:28:07 +0300

    make modal-dialog.js more self-contained

--- js/modal-dialog.js
+++ js/modal-dialog.js
@@ -5,7 +5,28 @@ export function isShowing() {
 }
 
 
+function setupHtml() {
+  if (document.getElementById('modal-dialog-container') === null) {
+    const div = document.createElement('div');
+    div.id = 'modal-dialog-container';
+    div.classList.add('hidden');
+    div.innerHTML =
+      '<div>' +
+      '  <div id="modal-dialog-top-bar">' +
+      '    <h2 id="modal-dialog-title"></h2>' +
+      '    <button id="modal-dialog-close-button">&#x274C;</button>' +
+      '  </div>' +
+      '  <div id="modal-dialog-content"></div>' +
+      '  <div id="modal-dialog-button-area"></div>' +
+      '</div>';
+    document.body.appendChild(div);
+  }
+}
+
+
 function showDialogWithCallback(titleText, dom, buttonTexts, callback) {
+  setupHtml();
+
   const titleElement = document.getElementById('modal-dialog-title');
   const containerDiv = document.getElementById('modal-dialog-container');
   const closeButton = document.getElementById('modal-dialog-close-button');
@@ -46,8 +67,3 @@ function showDialogWithCallback(titleText, dom, buttonTexts, callback) {
 export async function showDialog(titleText, dom, buttonTexts) {
   return new Promise(resolve => showDialogWithCallback(titleText, dom, buttonTexts, resolve));
 }
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  showDialog('Asd', document.createElement('div'), ['Lol'])
-});
